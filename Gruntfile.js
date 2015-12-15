3520911822b3786a532461a4f77da4cfd0d72468
nolte <karsten.nolte@itemis.de>
2015-12-15 15:22:08 +0100

    Gruntfile erweitert

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,6 +16,10 @@ module.exports = function(grunt) {
         // Project settings
         app: appConfig,
 
+        // Empties built folder to start fresh
+        clean: ['<%= app.built %>/*'],
+
+        // Compiles the sass files to normal css
         compass: {
             build: {
                 options: {
@@ -27,6 +31,22 @@ module.exports = function(grunt) {
             }
         },
 
+        // Post processing css incl. autoprefixer
+        postcss: {
+            options: {
+                map: true, // inline sourcemaps
+
+                processors: [
+                    require('autoprefixer')({browsers: 'last 2 versions'}), // add vendor prefixes
+                    require('cssnano')() // minify the result
+                ]
+            },
+            dist: {
+                src: '<%= app.css %>/*.css'
+            }
+        },
+
+        // Concatenates all css files -> style.css
         concat: {
             options: {
                 stripBanners: true,
@@ -34,13 +54,12 @@ module.exports = function(grunt) {
                 '<%= grunt.template.today("yyyy-mm-dd") %> */\n'
             },
             build: {
-                src: ['app/styles/css/*.css'],
-                dest: 'built/style.css'
+                src: ['<%= app.css %>/*.css'],
+                dest: '<%= app.dir %>/styles/style.css'
             }
         },
 
-        clean: ['<%= app.built %>/*'],
-
+        // Copy all files (html, css) into the built folder
         copy: {
             build: {
                 files: [
@@ -49,11 +68,18 @@ module.exports = function(grunt) {
                         cwd: 'app/',
                         src: ['*.html'],
                         dest: '<%= app.built %>'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'app/styles',
+                        src: ['style.css'],
+                        dest: '<%= app.built %>'
                     }
                 ]
             }
         },
 
+        // Filewatcher for html, css, js and livereload built files
         watch: {
             html: {
                 files: ['<%= app.dir %>/*.html'],
@@ -61,14 +87,23 @@ module.exports = function(grunt) {
             },
             css: {
                 files: ['<%= app.sass %>/*.{scss,sass}'],
-                tasks: ['compass', 'concat']
+                tasks: ['compass', 'postcss', 'concat', 'copy']
             },
             js: {
                 files: [],
                 tasks: []
+            },
+            livereload: {
+                options: {
+                    livereload: true
+                },
+                files: [
+                    '<%= app.built %>/{,*/}*.{html,css,js,png,jpg,jpeg,gif}'
+                ]
             }
         },
 
+        // Minifies js files
         uglify: {
             options: {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
@@ -85,12 +120,23 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     */
 
+    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-compass');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-postcss');
 
     // Default task(s).
-    grunt.registerTask('default', ['copy', 'compass', 'concat', 'watch']);
+    grunt.registerTask('default', [
+        'clean',
+        'copy',
+        'compass',
+        'postcss',
+        'concat',
+        'watch'
+    ]);
+
+
 };
